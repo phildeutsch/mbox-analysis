5f63bfb94e0766e0b295fe1c94da02c9c18c9415
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2014-03-12 15:00:21 +0100

    Update mbox-to-avro.py

--- mbox-to-avro.py
+++ mbox-to-avro.py
@@ -1,20 +1,24 @@
+import os
 import mailbox
 import avro.schema
 from avro.datafile import DataFileReader, DataFileWriter
 from avro.io import DatumReader, DatumWriter
 
 def get_body(message):
-    sm = str(message)
-    body_start = sm.find('iamunique', sm.find('iamunique')+1)
-    body_start = sm.find('Content-Transfer-Encoding', body_start+1)
-    body_start = sm.find('\n', body_start+1)+1
+    try:
+        sm = str(message)
+        body_start = sm.find('iamunique', sm.find('iamunique')+1)
+        body_start = sm.find('Content-Transfer-Encoding', body_start+1)
+        body_start = sm.find('\n', body_start+1)+1
+    
+        body_end = sm.find('From: ', body_start + 1)
+        body = sm[body_start:body_end]
 
-    body_end = sm.find('From: ', body_start + 1)
-    body = sm[body_start:body_end]
-
-    body = body.replace("=20\n", "")
-    body = body.replace("=FC", "ü")
-    body = body.replace("=", "")
+        body = body.replace("=20\n", "")
+        body = body.replace("=FC", "ü")
+        body = body.replace("=", "")
+    except:
+        body = "N/A"
     
     return body
 
@@ -29,15 +33,21 @@ def write_mbox(mboxfile, writer):
             'Subject': message['Subject'],
             'Body': get_body(message)
             })
+        print(message['Subject'])
    
 schema = avro.schema.Parse(open("email.avro.schema").read())
 writer = DataFileWriter(open("email.avro", "wb"), DatumWriter(), schema)
 
-mboxfile='mbox'
-write_mbox(mboxfile, writer)    
+path = './Archives'
+mboxfiles = [os.path.join(dirpath, f)
+	     for dirpath, dirnames, files in os.walk(path)
+	     for f in files if f.endswith('mbox')]
+for mboxfile in mboxfiles[:2]:
+    write_mbox(mboxfile, writer)
+    print(mboxfile)
 writer.close()
 
-reader = DataFileReader(open("email.avro", "rb"), DatumReader())
-for email in reader:
-    print(email['Date'])
-reader.close()
+#reader = DataFileReader(open("email.avro", "rb"), DatumReader())
+#for email in reader:
+#    print(email['Date'])
+#reader.close()
