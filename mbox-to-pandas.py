060c9046667ebcd631950fc13f3f997b4abe29e3
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2014-03-13 15:50:49 +0100

    Update mbox-to-pandas.py

--- mbox-to-pandas.py
+++ mbox-to-pandas.py
@@ -1,7 +1,47 @@
 import os
 import mailbox
+import re
+
+import avro.schema
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
 
 import pandas as pd
+
+def clean_address(address, lookupcsv):
+#    print('Dirty:\t' + address)
+    address = address.replace("<", "")
+    address = address.replace(">", "")
+    address = address.replace("\"", "")
+    address = address.replace("\n", " ")
+    address = address.replace("MAILER-DAEMON", "")
+    address = address.lower().strip()
+
+    with open(lookupcsv, 'rt') as lookupfile:
+        lookupdata = lookupfile.readlines()
+    for line in lookupdata:
+        name = line.split(',')[0]
+        if address == name:
+            address = line.split(',')[-1].strip()
+
+    email = None
+    for word in address.split(' '):
+        emailRegex = re.compile(
+            "^[a-zA-Z0-9._%-]+@[a-zA-Z0-9._%-]+.[a-zA-Z]{2,6}$"
+            )
+        email = re.match(emailRegex, word)
+        if email is not None:
+            cleanEmail = email.group(0)
+    if email is None:
+        if address.split(' ')[-1].find('@') > -1:
+            cleanEmail = address.split(' ')[-1].strip()
+        elif address.split(' ')[-1].find('?') > -1:
+            cleanEmail = 'n/a'
+        else:
+            cleanEmail = address
+              
+    print('Clean:\t' + cleanEmail)
+    return cleanEmail
 
 def get_body(message):
     try:
@@ -31,11 +71,12 @@ def get_body(message):
         body = "N/A"
     
     return body
-    
+
 def write_table(mboxfile, mailTable):
     for message in mailbox.mbox(mboxfile):
+        cleanFrom = clean_address(message['From'], 'name_to_address.csv')
         mailTable.append([
-            message['From'],
+            cleanFrom,
             message['To'],
             message['Cc'],
             message['Bcc'],
@@ -43,25 +84,21 @@ def write_table(mboxfile, mailTable):
             message['Subject'],
             get_body(message)
             ])
-        print(message['Subject'])
-        
-schema = avro.schema.Parse(open("email.avro.schema").read())
-writer = DataFileWriter(open("email.avro", "wb"), DatumWriter(), schema)
-
+   
 path = './Archives'
 mboxfiles = [os.path.join(dirpath, f)
 	     for dirpath, dirnames, files in os.walk(path)
 	     for f in files if f.endswith('mbox')]
 mailTable = []
 
-for mboxfile in mboxfiles:
-    print(mboxfile)
-    try:
-        write_table(mboxfile, mailTable)
-    except:
-        print('Error writing mbox to pandas.')
+for mboxfile in mboxfiles[:10]:
+#   print(mboxfile)
+    write_table(mboxfile, mailTable)
 writer.close()
 
 m = pd.DataFrame(mailTable)
 m.columns = ['From', 'To', 'Cc', 'Bcc', 'Date', 'Subject', 'Body']
-
+senders = m['From']
+counts = senders.value_counts()
+counts.sort()
+counts
